776ebb0866a8a695adf91d3abdeb7614b80dbe7b
QuentinLem <quentin.lemonnier.info@gmail.com>
2017-01-09 18:19:44 +0100

    reco différents objets (TODO:stock et save des obj dans saved.maps)

--- src/mapio.c
+++ src/mapio.c
@@ -6,113 +6,34 @@
 
 #include "map.h"
 #include "error.h"
+#include "list.h"
 
 #ifdef PADAWAN
 
-#define BUFFER_MAX_SIZE 200
+#define BUFFER_MAX_SIZE 250
 
-typedef struct Element Element;
-struct Element
-{
-   int value;
-   char description[BUFFER_MAX_SIZE];
-   Element *next;
-};
-
-typedef struct list_elem_map List;
-struct List
-{
-    Element *first;
-};
-
-List *list_init(int value, char description[BUFFER_MAX_SIZE])
-{
-   List *list = malloc(sizeof(struct List));
-   Element *element = malloc(sizeof(*element));
-   if (list == NULL || element == NULL)
-   {
-       exit(EXIT_FAILURE);
-   }
-   element->value = value;
-   sprintf(element->description, "%s", description);
-   element->next = NULL;
-   list->first = element;
-   return list;
-}
-
-
-void list_add_elem(List *list, int value, char* description)
-{
-   // Création du nouvel élémen
-   Element *newElem = malloc(sizeof(*newElem));
-   if (list == NULL || newElem == NULL)
-   {
-       exit(EXIT_FAILURE);
-   }
-   newElem->value = value;
-   sprintf(newElem->description, "%s", description);
-   // Insertion de l'élément au début de la liste
-   newElem->next = list->head;
-   list->head = newElem;
-
-}
-
-// Supression de l'element en tete de liste
-void list_delete_first_elem(List *list)
-{
-   if (list == NULL) {
-       exit(EXIT_FAILURE);
-   }
-   if (list->first != NULL)
-   {
-       Element *toDelete = list->first;
-       list->first = list->first->next;
-       free(toDelete);
-   }
-}
-
-// Renvoie la taille de notre liste chaînée
-int list_length(List *list)
-{
-    int length = 0;
-    while (list->head->next != NULL)  //tant que la fin de la liste n'est pas atteinte 
-     {
-       length++;
-       list = list->next;             //passer à l'élément suivant 
-     }
-     return length;
-}
-
-// Renvoie 1 si l'élément existe dans la liste, 0 sinon
-int list_search_element(List *list, int value){
-  List *tmp = list;
-  while(tmp->first->next!=NULL){
-    if(tmp->first->value == value){
+int isValueInArray(int val, int *array, int array_size){
+  for(int i = 0; i < array_size; i++){
+    if(array[i] == val){
       return 1;
-    } else {
-      tmp->first = tmp->first->next;
     }
   }
   return 0;
 }
 
-void list_toString(List *list) {
-   if (list == NULL) {
-       exit(EXIT_FAILURE);
-   }
-   char description[BUFFER_MAX_SIZE];
-   Element *current = list->first;
-   while (current != NULL) {
-       sprintf(description, "%s\n", current->description);
-       fprintf(stderr, description);
-       current = current->next;
-   }
-   printf("NULL\n");
+int save_obj(int *array, int obj_val, int iterator){
+  char* buffer[BUFFER_MAX_SIZE];
+  // si l'objet n'est as encore dans la liste
+  if(!isValueInArray(obj_val, array, iterator)) {
+    // stockage des données relatives à l'objet
+    array[iterator] = obj_val;
+    sprintf(buffer, "\n%d::%d:%d:%d:%d:%d:%d\n", obj_val, map_get_frames(obj_val), map_get_solidity(obj_val), map_is_collectible(obj_val), map_is_destructible(obj_val), map_is_generator(obj_val));
+    return 1;
+  }
+  return 0;
 }
-// */
 
-void map_new (unsigned width, unsigned height)
-{
+void map_new (unsigned width, unsigned height) {
   map_allocate (width, height);
 
   for (int x = 0; x < width; x++)
@@ -156,46 +77,46 @@ void map_save (char *filename)
   // TODO_begin
   char buffer[BUFFER_MAX_SIZE];
   char object_str_tmp[BUFFER_MAX_SIZE];
+  
+  int width = map_width();
+  int height = map_height();
+  int nb_obj = map_objects();
+
+  int objects_array[nb_obj];
   int object_value_tmp;
-  List *objects = NULL;
-  List *tmp;
+  int array_iterator = 0;
 
   int fd = open(filename, O_WRONLY|O_CREAT, 0666);
   if(fd == -1){
     exit_with_error("Map_save failed when opening file\n");
   }
   
-  // récupération variables d'environnement de map
-  int width = map_width();
-  int height = map_height();
-  int nb_obj = map_objects();
-  
   // concatenation pour write des variables 
   sprintf(buffer, "%d\n%d\n", width, height);
-  write(fd, &buffer, sizeof(char)*strlen(buffer));
+  if(write(fd, &buffer, sizeof(char)*strlen(buffer)) == -1){
+    exit_with_error ("Map save failed: write fct\n");
+  }
 
   // write de la matrice de la map
   for(int i=0; i<height; i++){
     for(int j=0; j<width; j++){
       object_value_tmp = map_get(j,i);
       sprintf(buffer, "%d ", object_value_tmp);
-      write(fd, &buffer, sizeof(char)*strlen(buffer));
+      if(write(fd, &buffer, sizeof(char)*strlen(buffer)) == -1){
+        exit_with_error ("Map save failed: write fct\n");
+      }
       
       // récupération des différents objets présents sur la map
-      // si la liste est vide
-      if(list_length(objects) == 0) {
-        objects = list_init(object_value_tmp, object_str_tmp);
-      // si l'objet n'est as encore dans la liste
-      } else if(!list_search_element(objects, object_value_tmp)) {
-        // stockage des données relatives à l'objet
-        sprintf(object_str_tmp, "%d::%d:%d:%d:%d:%d:%d", object_value_tmp, map_get_frames(object_value_tmp), map_get_solidity(object_value_tmp), map_is_collectible(object_value_tmp), map_is_destructible(object_value_tmp), map_is_generator(object_value_tmp));
-        list_add_elem(objects, object_value_tmp, object_str_tmp);
+      if(save_obj(objects_array, object_value_tmp, array_iterator)){
+        array_iterator++;
       }
     }
-    write(fd, "\n", sizeof(char));
+    if(write(fd, "\n", sizeof(char)) == -1){
+      exit_with_error ("Map save failed: write fct\n");
+    }
   }
 
-  int list_size = list_length(objects);
+  /*int list_size = list_length(objects);
   if(nb_obj != list_size){
     fprintf(stderr, "note: NB OBJECTS !");
     nb_obj = list_size;
@@ -205,34 +126,34 @@ void map_save (char *filename)
   for(int i=0; i<nb_obj; i++){
     fprintf(stderr, "%s", tmp->description);
     tmp->first = tmp->next;
-  }
+  }*/
 
-  //*/
+  //
 
   close(fd);
-  // TODO_end
-  fprintf(stderr, "Sorry: Map save is not yet implemented\n");
+  // TODO_end*/
+  fprintf(stderr, "Sorry : Map save is not totally implemented\n");
 }
 
 void map_load (char *filename)
 {
-  // TODO_begin
+  /*// TODO_begin
   char buffer;
   int fd = open(filename, O_WRONLY|O_CREAT, 0666);
   if(fd == -1){
     exit_with_error("Map_load failed when opening file\n");
   }
 
-  /*printf("")
+  printf("")
 
   for(i = input_length; i >=0 ; i--){
     lseek(fd1,i,SEEK_SET);
     read(fd1, &buffer, sizeof(char));
     write(fd2, &buffer, sizeof(char));
-  }*/
+  }
 
 
-  close(fd);
+  close(fd);*/
   // TODO_end
   exit_with_error ("Map load is not yet implemented\n");
 }
